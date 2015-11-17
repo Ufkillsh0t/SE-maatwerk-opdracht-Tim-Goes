73ac89474dc7133e923703d36b99b2c8d5b2a0af
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2015-11-17 08:44:51 +0100

    Score word nu weergeven

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+    public Text scoreText;
     public int score = 0;
 
 	// Use this for initialization
 	void Start () {
-
-	}
+        scoreText.text = "Score: " + score.ToString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,6 +20,7 @@ public class GameController : MonoBehaviour {
     public void AddScore(int scoreValue)
     {
         score += scoreValue;
+        scoreText.text = "Score: " + score.ToString();
         Debug.Log("[Score] " + score);
     }
 }
